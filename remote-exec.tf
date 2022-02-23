3a3553a25422ae9772bfedf51f68c8fa257ad726
mostafa <mostafaa.abdallaa@gmail.com>
2022-02-23 13:14:54 +0000

    edited the ec2 ami in the configuration file

--- remote-exec.tf
+++ remote-exec.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "myec2" {
-  ami                    = "ami-08e4e35cccc6189f4"
+  ami                    = "ami-033b95fb8079dc481"
   instance_type          = "t2.micro"
   key_name               = "ec2-keypair"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
